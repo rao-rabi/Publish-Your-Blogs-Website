e8d3ac6ab5fd0182fa394136d1f327bb52d47f02
rao-rabi <rabirao1039@gmail.com>
2023-11-28 12:03:31 +0500

    Components almsot developed

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,9 @@ import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Logo from "./Logo.jsx";
-import Container from './container/Container.jsx'
+import RTE from "./RTE.jsx";
+import Container from "./container/Container.jsx";
+import Input from "./Input.jsx";
+import Select from "./Select.jsx";
 
-
-
-export {Header, Footer, LogoutBtn, Container, Logo}
+export { Header, Footer, LogoutBtn, Container, Logo, Input, Select, RTE };
---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,22 @@
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Button, RTE, Select, Input } from "../index";
+import appwriteService from "../../appwrite/config/config";
+
+export default function PostForm({ post }) {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.$id || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  return <div>PostForm</div>;
+}
