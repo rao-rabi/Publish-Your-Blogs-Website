ab2c9433610856fa2ce94af12d7a469be93e6f51
rao-rabi <rabirao1039@gmail.com>
2023-12-02 18:24:14 +0500

    pages developed

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,9 @@ import RTE from "./RTE.jsx";
 import Container from "./container/Container.jsx";
 import Input from "./Input.jsx";
 import Select from "./Select.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import PostForm from './post-form/PostForm.jsx'
+import PostCard from './PostCard.jsx;'
 
-export { Header, Footer, LogoutBtn, Container, Logo, Input, Select, RTE };
+export { Header, Footer, LogoutBtn, Container, Logo, Input, Select, RTE, Signup, Login, PostForm, PostCard};
---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Container, PostFrom } from "../components";
+import appwriteService from "../appwrite/config/config";
+import { useNavigate, useParams } from "react-router-dom";
+
+function EditPost() {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostFrom post={post} />
+      </Container>
+    </div>
+  ) : null;
+}
+export default EditPost;
